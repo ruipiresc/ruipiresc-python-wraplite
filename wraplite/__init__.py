87045464a606875c415465aee92852de4148c1eb
rui.pires <rui.pires@accenture.com>
2020-08-06 11:03:50 +0100

    optimization of __init__.py

--- wraplite/__init__.py
+++ wraplite/__init__.py
@@ -1,19 +1,13 @@
 import os
 from wraplite import exceptions
 from wraplite.database import Database
-from wraplite.table import TableFormat
 
-def get(name: str, folder_path: str = None, error_if_exist: bool = False) -> bool:
-    if folder_path is None:
-        folder_path = './'
-
+def get(name: str, folder_path: str = './', error_if_exist: bool = False) -> bool:
     if not os.path.isdir(folder_path):
         raise exceptions.FolderNotExistError()
 
-    database_path = os.path.join(folder_path, name + '.db')
-
     if error_if_exist:
-        if os.path.exists(database_path):
+        if os.path.exists(os.path.join(folder_path, name + '.db')):
             raise exceptions.DatabaseExistError()
 
     return Database(name, folder_path)
