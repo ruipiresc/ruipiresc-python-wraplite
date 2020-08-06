a372ac74ed2dbb6e57e62c99adc1321efa964504
rui.pires <rui.pires@accenture.com>
2020-08-06 11:03:21 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'wraplite', '__version__.py')) as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name=about['__title__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=['wraplite'],
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=['pandas'],
+    license=about['__license__'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: SQL',
+        'Topic :: Utilities'
+    ],
+    keywords='nosql sqlite wrapper wraplite sql'
+)
