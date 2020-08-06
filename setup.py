097a58ca98aea85919ef69029def2cf3ac95e770
rui.pires <rui.pires@accenture.com>
2020-08-06 11:38:06 +0100

    add git push

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ def publish(option: str = 'build') -> None:
     #os.system('python -m bumpversion ' + option)
     os.system('python setup.py build')
     os.system('twine upload dist/*')
-    os.system('git push --tags ')
+    os.system('git push')
+    os.system('git push --tags')
     sys.exit()
     
 if len(sys.argv) > 1:
